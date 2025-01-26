152fe771485b7b2f8ce9044994c42f589d2f76c1
Dave Clayton <daveclayton@users.noreply.github.com>
2025-01-26 12:07:34 +0000

    Fix callback URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   "clientId": "-V_uK6mE1LMtE0MiNBFur2i-M94jhv7Z8JQm9YPpzRg",
-  "redirectUri": process.env.LOCAL_URL || "https://daveclayton.github.io/planner-for-ynab/"
+  "redirectUri": process.env.CALLBACK_URL
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,9 @@ import dotenv from "dotenv";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config();
+dotenv.config({ 
+  path: ['.env.local', '.env'] 
+});
 
 export default {
   entry: "./src/main.js",
